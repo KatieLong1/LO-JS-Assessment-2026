403dd8654bb53076cde0719210c4e8550724c48b
KatieLong1 <45803365+KatieLong1@users.noreply.github.com>
2026-04-13 20:16:26 +1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+/********************** 
+ * Cupcake ordering App
+**********************/
+
+/*****Variables*******/
+
+
+
+/********Main Code*******/
+
+
+Const OUTPUT = document.getElementById("JavaScriptOutput");
+OUTPUT.innerHTML = "<h2>Added by JS</h2>"
