d02f53fb5b9ebab561f9b7b9fc21e9b65d540c19
KatieLong1 <45803365+KatieLong1@users.noreply.github.com>
2026-04-14 03:25:22 +0000

    Added array

--- script.js
+++ script.js
@@ -2,12 +2,23 @@
  * Cupcake ordering App
 **********************/
 
+console.log("Running Script.js");
+console.log("We are learning JS");
+
 /*****Variables*******/
 
-
+let userName = "Katie";
+let cakeArray = ["Chocolate - $5", "Orange - $4", "Lemon - $3.40"];
 
 /********Main Code*******/
 
 
-Const OUTPUT = document.getElementById("JavaScriptOutput");
-OUTPUT.innerHTML = "<h2>Added by JS</h2>"
+const OUTPUT = document.getElementById("JavaScriptOutput");
+OUTPUT.innerHTML = "<h2> Hi "  + userName +", " + " what would you like to order today? </h2>";
+OUTPUT.innerHTML += "<p> Our specials of the day are: " + cakeArray + "</p>"; 
+
+function getUserName(){
+    const NAME_FIELD = document.getElementById("nameField");
+    let userName= NAME_FIELD.value;
+    OUTPUT.innerHTML += "<p>Your name is " + userName + "</p>";
+}
